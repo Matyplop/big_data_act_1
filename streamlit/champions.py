d1dd2e5d05ebd6ad6cfe0bf369fa6c2db9214d4c
Arquero <PC@arch.linux>
2024-08-05 18:14:30 -0400

    inital commit

---
+++ streamlit/champions.py
@@ -0,0 +1,119 @@
+import streamlit as st
+from functions import *
+import pandas as pd
+import time
+encabezado = ["Nombre", "Rol", "Vida base",
+              "Mana base", "Armadura base", "Daño ataque base", "Eficiencia de Oro"]
+
+roles_disponibles = ['Support', 'Mid', 'ADC', 'Jungle', 'Top']
+
+
+logo = st.sidebar.image("logito.png", width=200)
+
+
+df = pd.read_csv("../champions.csv", names=encabezado, skiprows=1)
+
+
+st.write("### Bienvenido a la base de datos de campeones de LoL")
+options = ['Selecciona una opción', 'Ingresar nuevo campeón',
+           'Filtrar por datos', 'Modificar campeón existente']
+selected = st.sidebar.selectbox(
+    '¿Que acción desea realizar?', options)
+
+
+if selected == options[1]:
+    df_show = st.dataframe(df)
+    with st.sidebar.form(key='my_form'):
+
+        Nombre = st.text_input(
+
+            "Ingresa nombre",
+        )
+        Rol = st.text_input(
+            "Ingresa Rol",
+        )
+        Vida_base = st.text_input(
+            "Ingresa Vida base",
+        )
+
+        Mana_base = st.text_input(
+            "Ingresa Mana base",
+        )
+
+        Armadura_base = st.text_input(
+            "Ingresa Armadura base",
+        )
+
+        Daño_ataque_base = st.text_input(
+            "Ingresa daño de ataque base ",
+        )
+
+        Eficiencia_de_oro = st.text_input(
+            "Ingresa Eficiencia de oro",
+        )
+        submit_button = st.form_submit_button(label='Agregar')
+    if submit_button:
+        df2 = agregar_campeon(Nombre, Rol, Vida_base, Mana_base,
+                              Armadura_base, Daño_ataque_base, Eficiencia_de_oro)
+        df2.to_csv('../champions.csv', mode='w', index=False)
+        df_show.empty()
+        df2
+
+elif selected == options[2]:
+    with st.sidebar.form(key='my_form'):
+        selected_type = st.selectbox(
+            'Seleccione tipo de dato', encabezado)
+
+        data = st.text_input(
+            "Ingrese parametro de busqueda",
+        )
+        submit_button2 = st.form_submit_button(label='Aceptar')
+    if submit_button2:
+        if selected_type == encabezado[6]:
+            data = float(data)
+        elif selected_type != encabezado[0] and selected_type != encabezado[1]:
+            data = int(data)
+        df3 = buscar_campeon(selected_type, data)
+        df3
+
+elif selected == options[3]:
+    with st.sidebar.form(key='my_form'):
+        seleccionar_campeon = st.selectbox(
+            "Selecciona Campeón a modificar", df["Nombre"].unique())
+
+        if seleccionar_campeon:
+            # que empiece en la primera fila , en donde está el campeon
+            champion_data = df[df['Nombre'] == seleccionar_campeon].iloc[0]
+            rol = st.selectbox(
+                "Rol", roles_disponibles, index=roles_disponibles.index(champion_data["Rol"]))
+            vida_base = st.number_input(
+                "Vida base", value=champion_data["Vida base"])
+            mana_base = st.number_input(
+                "Mana base", value=champion_data["Mana base"])
+            armadura_base = st.number_input(
+                "Armadura base", value=champion_data["Armadura base"])
+            dano_ataque_base = st.number_input(
+                "Daño ataque base", value=champion_data["Daño ataque base"])
+            eficiencia_oro = st.number_input(
+                "Eficiencia de Oro", value=champion_data["Eficiencia de Oro"])
+            submit_button3 = st.form_submit_button(label='Guardar cambios')
+
+        if submit_button3:
+            with st.spinner("Generando cambios.."):
+                time.sleep(1)
+                st.balloons()
+
+            df.loc[df['Nombre'] == seleccionar_campeon, 'Vida base'] = vida_base
+            df.loc[df['Nombre'] == seleccionar_campeon, 'Rol'] = rol
+            df.loc[df['Nombre'] == seleccionar_campeon, 'Mana base'] = mana_base
+            df.loc[df['Nombre'] == seleccionar_campeon,
+                   'Armadura base'] = armadura_base
+            df.loc[df['Nombre'] == seleccionar_campeon,
+                   'Daño ataque base'] = dano_ataque_base
+            df.loc[df['Nombre'] == seleccionar_campeon,
+                   'Eficiencia de Oro'] = eficiencia_oro
+
+            # el "w" pensé que funcionaba pero no
+            df.to_csv("../champions.csv", mode='w', index=False)
+
+    st.dataframe(df)  # ahi lo dejé afuera
---
+++ streamlit/functions.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import streamlit as st
+
+
+def reload_dataframe(df):
+    df
+
+
+def agregar_campeon(nombre, rol, vida_base, mana_base, armadura_base, daño_ataque_base, oro):
+
+    encabezado = ["Nombre", "Rol", "Vida base",
+                  "Mana base", "Armadura base", "Daño ataque base", "Eficiencia de Oro"]
+
+    df = pd.read_csv("../champions.csv", names=encabezado, skiprows=1)
+    campeon = pd.DataFrame({
+        'Nombre': [nombre],
+        'Rol': [rol],
+        'Vida base': [vida_base],
+        'Mana base': [mana_base],
+        'Armadura base': [armadura_base],
+        'Daño ataque base': [daño_ataque_base],
+        'Eficiencia de Oro': [oro],
+    })
+    # aqui lo que quiero hacer es añadir lo de arriba al dataframe original , pero el append no funciona entonces uso el concat
+    df2 = pd.concat([df, campeon], ignore_index=True)
+    return df2
+
+
+def buscar_campeon(selected_type, data):
+    encabezado = ["Nombre", "Rol", "Vida base",
+                  "Mana base", "Armadura base", "Daño ataque base", "Eficiencia de Oro"]
+
+    df = pd.read_csv("../champions.csv", names=encabezado, skiprows=1)
+    if type(data) is not str:
+        resultados = df[df[selected_type] == data]
+    else:
+        resultados = df[df[selected_type].str.contains(
+            data, case=False, na=False)]
+
+    if not resultados.empty:
+        print(f"Se encontraron {len(resultados)} resultados:")
+        print(resultados)
+    else:
+        print("No se encontró el Campeón.")
+
+    return resultados
+
+
+# def modificar_campeon(selected_type,nombre_original):
+    # encabezado = ["Nombre", "Rol", "Vida base",
+    #               "Mana base", "Armadura base", "Daño ataque base", "Eficiencia de Oro"]
+    #
+    # df = pd.read_csv("../champions.csv", names=encabezado, skiprows=1)
+    #
+    # campeon_existente = df[df[selected_type].str.contains(
+    #     nombre_original, case=False, na=False)]
+    #
+    # if campeon_existente.empty:
+    #     print(f"No se encontró el Campeón '{nombre_original}'.")
+    #     return
+    #
+    # return campeon_existente
+    #
+    # if nuevo_nombre:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Nombre'] = nuevo_nombre
+    # if nuevo_rol:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Rol'] = nuevo_rol
+    # if nuevo_hp:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Vida base'] = nuevo_hp
+    # if nuevo_ap:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Mana base'] = nuevo_ap
+    # if nuevo_armor:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Armadura base'] = nuevo_armor
+    # if nuevo_attack:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Daño ataque base'] = nuevo_attack
+    #
+    # if nuevo_Oro:
+    #     df.loc[df['Nombre'].str.contains(
+    #         nombre_original, case=False, na=False), 'Oro'] = nuevo_Oro
